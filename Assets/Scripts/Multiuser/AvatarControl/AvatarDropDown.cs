ac13bf6f5456b56876dda1d66ece4dc3374a8b46
Henrynaut <neilmchenry@gmail.com>
2021-08-15 11:41:33 -0500

    Added avatar selection options in UI dropdown menu

--- Assets/Scripts/Multiuser/AvatarControl/AvatarDropDown.cs
+++ Assets/Scripts/Multiuser/AvatarControl/AvatarDropDown.cs
@@ -7,10 +7,10 @@ public class AvatarDropDown : MonoBehaviour
 {
     List<string> labels = new List<string>() {
     "Please Select",
-    "Pre-Questionnaire",
-    "Training Scenario",
-    "Rover Repair",
-    "Post-Questionnaire"
+    "Robot",
+    "Llama",
+    "Duck",
+    "Shiba"
     };
 
     public TMP_Dropdown dropdown;
@@ -23,24 +23,24 @@ public class AvatarDropDown : MonoBehaviour
         MenuController.MC.OnClickCharacterPick(index);
         Debug.Log(labels[index]);
 
-        // Open Pre-Questionairre if label 1 selected
-        if (labels[index] == labels[1])
-        {
-            Application.OpenURL("https://forms.gle/6YMJJJjBC1FkjCtp6");
-        }
-        // To do: Add a confirm button
+        // // Open Pre-Questionairre if label 1 selected
+        // if (labels[index] == labels[1])
+        // {
+        //     Application.OpenURL("https://forms.gle/6YMJJJjBC1FkjCtp6");
+        // }
+        // // To do: Add a confirm button
 
-        // Open training video if label 2 selected
-        if (labels[index] == labels[2])
-        {
-            Application.OpenURL("https://youtu.be/yEqyRM5Hu2g");
-        }
+        // // Open training video if label 2 selected
+        // if (labels[index] == labels[2])
+        // {
+        //     Application.OpenURL("https://youtu.be/yEqyRM5Hu2g");
+        // }
 
-        // Open Post-Questionairre if label 4 selected
-        if (labels[index] == labels[4])
-        {
-            Application.OpenURL("https://forms.gle/vicwfKatg1bKDuNk8");
-        }
+        // // Open Post-Questionairre if label 4 selected
+        // if (labels[index] == labels[4])
+        // {
+        //     Application.OpenURL("https://forms.gle/vicwfKatg1bKDuNk8");
+        // }
     }
     
     void Start(){
